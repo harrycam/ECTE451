2a6876d59dddbeb3af9e1c2e2573befaf01ccae2
harrycam <66153100+harrycam@users.noreply.github.com>
2020-10-31 14:27:59 +1100

    Update createRobotAgent.m

--- createRobotAgent.m
+++ createRobotAgent.m
@@ -13,35 +13,39 @@ Tf = 30;    % Simulation end time
 useFastRestart = false;
 
 % Create the observation info
-numObs = 6;
+numObs = 6; % Number of observation inputs
 observationInfo = rlNumericSpec([numObs 1]);
 observationInfo.Name = 'observations';
 
 % create the action info
-numAct = 2;
+numAct = 2; % Number of action outputs
 actionInfo = rlNumericSpec([numAct 1],'LowerLimit',-2,'UpperLimit', 2);
 actionInfo.Name = 'wheel_velocity';
 % Environment
 
-mdl = 'RoboBlockRL';
+mdl = 'RoboBlockRL'; % Name of Simulink file
 load_system(mdl);
 blk = [mdl,'/RL Agent'];
 env = rlSimulinkEnv(mdl,blk,observationInfo,actionInfo);
-env.ResetFcn = @(in)ResetFcn(in);
+env.ResetFcn = @(in)ResetFcn(in); % Initialise reset function to specify starting location
 %function to allow changing of parameters on reset - investigate this
 
 if ~useFastRestart
    env.UseFastRestart = 'off';
 end
 %% CREATE NEURAL NETWORKS
-createDDPGNetworks;
+
+createDDPGNetworks; % Create NN representations of actor and critic
                      
 %% CREATE AND TRAIN AGENT
-createDDPGOptions;
-agent = rlDDPGAgent(actor,critic,agentOptions);
-trainingResults = train(agent,env,trainingOptions)
-%exp1 = sim(agent,env)
+
+createDDPGOptions; % Options for RL
+agent = rlDDPGAgent(actor,critic,agentOptions); % Initialise learning agent from NNs
+trainingResults = train(agent,env,trainingOptions) % Train agent with RL algorithm
+%exp1 = sim(agent,env) % Simulate trained agent
+
 %% SAVE AGENT
+
 reset(agent); % Clears the experience buffer
 curDir = pwd;
 saveDir = 'savedAgents';
